d28a1439438db65c614a97092d7ab6b061ca97d7
shypie <shilynnbf@gmail.com>
2026-06-17 16:44:21 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,19 @@
+full_name = "Shilynn Ba"
+
+nickname = "Shy"
+
+age = "17"
+
+used_python_before = False
+
+hobbies= ["listening to music", "sewing", "watching tv"]
+
+favorites = { "movie": "avatar 3", "food": "cucumber", "music": "shoesgaze"}
+
+print(full_name,age,hobbies)
+
+skills = ("drawing","eating") # tuple: ordered, cannot change
+
+languages_spoken = {"English","Chinese"} # set: no duplicates
+
+print(languages_spoken)
